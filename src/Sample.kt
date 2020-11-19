e3429f5a89b6721cb05217fd59187ebdef557449
kousairikuri <iikura@ss.cs.osakafu-u.ac.jp>
2020-11-19 17:05:32 +0900

    Make it possible to find next Repunit prime number of 11

--- src/Sample.kt
+++ src/Sample.kt
@@ -1,18 +1,22 @@
 import kotlin.math.*
 
 fun isPrime(x:Long): Boolean{
-    if(x==1L) return false
+    if (x == 1L) return false
+    else if (x == 2L) return true
+    else if (x / 2L == 0L) return false
 
-    for (j in 2L..x / 2) {
-            if (x % j == 0L) {
-                return false
-            }
+    var j = 3L
+    while (j <= sqrt(x.toDouble())) {
+        if (x % j == 0L) {
+            return false
+        }
+        j += 2L
     }
     return true
 }
 fun main() {
-    for (i in 1L..20L) {
-        var n = (10.0.pow(i.toDouble()) / 9).toLong()
+    for (i in 1..19) {
+        var n = "1".repeat(i).toLong()
         println("%d %b".format(n, isPrime(n)))
     }
 }
