c95e4c791903e078397692da9eba701af1b46cea
sab01101 <73996457+sab01101@users.noreply.github.com>
2020-11-19 19:46:40 +0900

    Update Sample.kt

--- src/Sample.kt
+++ src/Sample.kt
@@ -3,16 +3,17 @@ import kotlin.math.*
 fun isPrime(x:Long): Boolean{
     if(x==1L) return false
 
-    for (j in 2L..x / 2) {
-            if (x % j == 0L) {
-                return false
-            }
+    for (j in 2L..(sqrt(x.toDouble())).toLong()) {
+        if (x % j == 0L) {
+            return false
+        }
     }
     return true
 }
 fun main() {
-    for (i in 1L..20L) {
-        var n = (10.0.pow(i.toDouble()) / 9).toLong()
+    var n = 0L
+    for(i in 1L..19L){
+        n += (10.0.pow(i.toDouble()-1.0)).toLong()
         println("%d %b".format(n, isPrime(n)))
     }
 }
