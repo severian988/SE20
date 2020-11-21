574b9731fc982195c26cc5c511d3a997e3f0736c
Leestalion <74027214+Leestalion@users.noreply.github.com>
2020-11-21 15:42:15 +0100

    Optimisation for large integer
    Optimisation of the code for large integer, The code now work for the program for i from 1 to 20.

--- src/Sample.kt
+++ src/Sample.kt
@@ -1,18 +1,27 @@
 import kotlin.math.*
+import java.math.BigInteger
 
-fun isPrime(x:Long): Boolean{
-    if(x==1L) return false
-
-    for (j in 2L..x / 2) {
-            if (x % j == 0L) {
-                return false
-            }
+fun isPrime(x:BigInteger): Boolean{
+    if (x==2.toBigInteger() || x==3.toBigInteger()) return true
+    if(x==1.toBigInteger() || x%2.toBigInteger()==0.toBigInteger() || x%3.toBigInteger()==0.toBigInteger()) return false
+    var j = 5.toBigInteger()
+    while (j < x.sqrt()) {
+        if (x % j == 0.toBigInteger() || x % (j+2.toBigInteger()) == 0.toBigInteger()) {
+            return false
+        }
+        j+=6.toBigInteger()
     }
     return true
 }
 fun main() {
-    for (i in 1L..20L) {
-        var n = (10.0.pow(i.toDouble()) / 9).toLong()
-        println("%d %b".format(n, isPrime(n)))
+    for (i in 1..20) {
+        var n = ((10.toBigDecimal().pow(i)) / 9.toBigDecimal()).toBigInteger()
+        if (isPrime(i.toBigInteger())){
+            println("R = %d : %d %b".format(i, n, isPrime(n)))
+        }
+        else{
+            println("R = %d : %d %b".format(i, n, false))
+        }
+
     }
 }
