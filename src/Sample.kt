4ab71d9cec911416a4ef6bebaf2115974c4b0290
Johan Berdougo <johan.berdougo@gmail.com>
2020-11-21 22:25:40 +0900

    My work on sample.kt

--- src/Sample.kt
+++ src/Sample.kt
@@ -1,18 +1,25 @@
-import kotlin.math.*
+import java.math.BigInteger
 
-fun isPrime(x:Long): Boolean{
-    if(x==1L) return false
+fun isPrime(x:BigInteger): Boolean{
+    // 1 is not Prime
+    if (x==1.toBigInteger()) return false
+    // 2 and 3 are Prime
+    if (x==2.toBigInteger() || x==3.toBigInteger()) return true
 
-    for (j in 2L..x / 2) {
-            if (x % j == 0L) {
-                return false
-            }
+    // Checking if x is a multiple of 2 or 3  -> not Prime
+    if (x % 2.toBigInteger() == 0.toBigInteger() || x % 2.toBigInteger() == 0.toBigInteger()) return false
+
+    // Beginning the loop at 5
+    var i = 5.toBigInteger()
+    while( i <= x.sqrt() ){
+        if (x % i == 0.toBigInteger() || x % (i+2.toBigInteger()) == 0.toBigInteger()) return false
+        i += 6.toBigInteger()
     }
     return true
 }
 fun main() {
-    for (i in 1L..20L) {
-        var n = (10.0.pow(i.toDouble()) / 9).toLong()
-        println("%d %b".format(n, isPrime(n)))
+    for (i in 1..20) {
+        var n = ((10.toBigDecimal().pow(i)-1.toBigDecimal()) / 9.toBigDecimal()).toBigInteger()
+        println("R%d - %d %b".format(i, n, isPrime(n)))
     }
 }
