5b52b739c329057e03c633e183ca136dae54797b
Johan Berdougo <johan.berdougo@gmail.com>
2020-11-21 22:53:20 +0900

    My work on sample.kt

--- src/Sample.kt
+++ src/Sample.kt
@@ -7,7 +7,7 @@ fun isPrime(x:BigInteger): Boolean{
     if (x==2.toBigInteger() || x==3.toBigInteger()) return true
 
     // Checking if x is a multiple of 2 or 3  -> not Prime
-    if (x % 2.toBigInteger() == 0.toBigInteger() || x % 2.toBigInteger() == 0.toBigInteger()) return false
+    if (x % 2.toBigInteger() == 0.toBigInteger() || x % 3.toBigInteger() == 0.toBigInteger()) return false
 
     // Beginning the loop at 5
     var i = 5.toBigInteger()
@@ -18,8 +18,13 @@ fun isPrime(x:BigInteger): Boolean{
     return true
 }
 fun main() {
-    for (i in 1..20) {
-        var n = ((10.toBigDecimal().pow(i)-1.toBigDecimal()) / 9.toBigDecimal()).toBigInteger()
-        println("R%d - %d %b".format(i, n, isPrime(n)))
+    // Printing the Rn from 1 to 20 with if it is prime or not
+    for (n in 1..20) {
+        var r = ((10.toBigDecimal().pow(n)-1.toBigDecimal()) / 9.toBigDecimal()).toBigInteger()
+        if (isPrime(n.toBigInteger())) {
+            println("R%d - %d %b".format(n, r, isPrime(r)))
+        } else{
+            println("R%d - %d %s".format(n, r, "false"))
+        }
     }
 }
