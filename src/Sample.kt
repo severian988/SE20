c44d0e6c0b7a0e7cd9f930047788d445f77c6239
4g-kojima <kojima@sig.cs.osakafu-u.ac.jp>
2020-11-22 17:26:08 +0900

    add my solution

--- src/Sample.kt
+++ src/Sample.kt
@@ -1,18 +1,39 @@
 import kotlin.math.*
 
-fun isPrime(x:Long): Boolean{
-    if(x==1L) return false
+fun Fermat(x:Double): Boolean{
+    if(x==1.0) {
+        return false
+    }
+    var n = 0
+    val y = 2
 
-    for (j in 2L..x / 2) {
-            if (x % j == 0L) {
-                return false
-            }
+
+        if(2.0.pow((x-1).toDouble()) % x == 1.0) {
+            return true
+        }
+    return false
+}
+
+fun isPrime(x:Long): Boolean {
+    if (x == 1L) return false
+
+    for (j in 2L..(sqrt(x.toDouble())).toLong()) {
+        if (x % j == 0L) {
+            return false
+        }
     }
     return true
 }
+
 fun main() {
-    for (i in 1L..20L) {
-        var n = (10.0.pow(i.toDouble()) / 9).toLong()
-        println("%d %b".format(n, isPrime(n)))
+    var n = 0L
+    for(i in 1..19){
+        n = n * 10L + 1L
+        println("%d %d %b".format(i, n, Fermat(n.toDouble())))
+    }
+    n = 0L
+    for(i in 1..19){
+        n = n * 10L + 1L
+        println("%d %d %b".format(i, n, isPrime(n)))
     }
 }
