5502bb972b2b3b518bac00d9f9aa35397f3a2226
tnakai11 <tnakai@m.cs.osakafu-u.ac.jp>
2020-11-23 22:35:16 +0900

    make it faster to find composite number

--- src/Sample.kt
+++ src/Sample.kt
@@ -4,20 +4,23 @@ import java.math.BigInteger
 fun isPrime(x:BigInteger): Boolean{
     if(x==1.toBigInteger()) return false
     if(x==2.toBigInteger()) return true
-
     if(x % 2.toBigInteger() == 0.toBigInteger()) return false
-    var j = 3.toBigInteger()
-    while( j <= x.sqrt() ){
-        if (x % j == 0.toBigInteger() ) return false
-        j += 2.toBigInteger()
+    for (i in 3L..x.sqrt().toLong() step 2L){
+        if(x % i.toBigInteger() == 0.toBigInteger()){
+            return false
+        }
     }
     return true
 }
 fun main() {
     var nMinusOne = 0.toBigInteger()
-    for (i in 1L..20L) {
+    for (i in 1L..22L) {
         val n = nMinusOne + 1.toBigInteger()
-        println("%d %b".format(n, isPrime(n)))
+        var ans = false
+        if( isPrime(n.toString().length.toBigInteger()) ){
+            ans = isPrime(n)
+        }
+        println("%d %b".format(n,ans) )
         nMinusOne = n * 10.toBigInteger()
     }
 }
