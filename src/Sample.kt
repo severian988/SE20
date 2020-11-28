c26a5f4dbf23a5335e4c69e5c6bc95a20a9ff360
kousairikuri <iikura@ss.cs.osakafu-u.ac.jp>
2020-11-29 00:23:27 +0900

    add a few changes

--- src/Sample.kt
+++ src/Sample.kt
@@ -74,33 +74,17 @@ fun isPrimeRepunit(x:BigInteger): Boolean{
 }
 
 fun isPrime(x:Long): Boolean{
-    if (x == 1L) return false
-    else if (x == 2L) return true
-    else if (x / 2L == 0L) return false
+    if(x==1L) return false
 
-<<<<<<< HEAD
-    var j = 3L
-    while (j <= sqrt(x.toDouble())) {
-        if (x % j == 0L) {
-            return false
-        }
-        j += 2L
-=======
     var j = 2L
     while(j*j<=x){
         if (x % j == 0L) return false
         j++
->>>>>>> nanai-branch
     }
     return true
 }
 
 fun main() {
-<<<<<<< HEAD
-    for (i in 1..19) {
-        var n = "1".repeat(i).toLong()
-        println("%d %b".format(n, isPrime(n)))
-=======
     val time = measureTimeMillis {
         var n = 1.toBigInteger()
 
@@ -112,7 +96,6 @@ fun main() {
             }
             n = n * 10.toBigInteger() + 1.toBigInteger()
         }
->>>>>>> nanai-branch
     }
     println("$time ms")
 }
