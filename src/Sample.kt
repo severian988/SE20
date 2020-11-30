fe695ef22f31846fdc23e6c20f5990541e544594
ebrosset <74173436+ebrosset@users.noreply.github.com>
2020-11-30 17:09:44 +0900

    Update Sample.kt
    Added lower and upper bound paramaters as val arguments.

--- src/Sample.kt
+++ src/Sample.kt
@@ -3,36 +3,32 @@ import kotlin.system.measureTimeMillis
 fun main() {
     val checker = Checker()
     val time = measureTimeMillis {
-        var n = 1.toBigInteger()
+
+        val i_min = 49081L   //Lower bound
+        val i_max = 49085L   //Upper bound
+        var n = (10.toBigInteger().pow((i_min).toInt())-1.toBigInteger()).divide(9.toBigInteger())
 
         var primeRepunits = mutableListOf<Long>()
-        var numTrue = 0L
-        var numFalse = 0L
 
-        for (i in 1L..1031L) {
+        for (i in i_min..i_max) {
             if(checker.isPrime(i)){
                 //println("R_$i %b".format(checker.isPrimeRepunit(n)))
                 if(checker.isPrimeRepunit(n)){
-                    numTrue += 1
                     primeRepunits.add(i) //Add index of repunit n
-                }
-                else{
-                    numFalse += 1
                 }
             }else{
                 //println("R_$i false")
-                numFalse += 1
             }
             n = n * 10.toBigInteger() + 1.toBigInteger()
 
             //Every 500 repunits tested, report progress
-            if(i%500 == 499L) {
-                println("Last tested repunit R_$i; $numTrue prime numbers found")
+            if(i%100 == 0L) {
+                println("Last tested repunit R_$i; ${primeRepunits.size} total prime repunit found")
             }
         }
 
         //Once all repunits were tested, print final results :
-        println("\n Prime repunits found are : :")
+        println("\n Prime repunits found are :")
         for(i in 0..primeRepunits.size-1){
             println("R_${primeRepunits[i]} is prime")
         }
