89cffec60ef7914544fdf497ed616bbc599c029b
ebrosset <74173436+ebrosset@users.noreply.github.com>
2020-11-30 12:33:30 +0900

    Update Sample.kt

--- src/Sample.kt
+++ src/Sample.kt
@@ -1,5 +1,3 @@
-import java.math.BigInteger
-import kotlin.math.*
 import kotlin.system.measureTimeMillis
 
 fun main() {
@@ -7,14 +5,37 @@ fun main() {
     val time = measureTimeMillis {
         var n = 1.toBigInteger()
 
-        for (i in 1L..317L) {
+        var primeRepunits = mutableListOf<Long>()
+        var numTrue = 0L
+        var numFalse = 0L
+
+        for (i in 1L..1031L) {
             if(checker.isPrime(i)){
-                println("R_$i %b".format(checker.isPrimeRepunit(n)))
+                //println("R_$i %b".format(checker.isPrimeRepunit(n)))
+                if(checker.isPrimeRepunit(n)){
+                    numTrue += 1
+                    primeRepunits.add(i) //Add index of repunit n
+                }
+                else{
+                    numFalse += 1
+                }
             }else{
-                println("R_$i false")
+                //println("R_$i false")
+                numFalse += 1
             }
             n = n * 10.toBigInteger() + 1.toBigInteger()
+
+            //Every 500 repunits tested, report progress
+            if(i%500 == 499L) {
+                println("Last tested repunit R_$i; $numTrue prime numbers found")
+            }
+        }
+
+        //Once all repunits were tested, print final results :
+        println("\n Prime repunits found are : :")
+        for(i in 1..primeRepunits.size-1){
+            println("R_${primeRepunits[i]} is prime")
         }
     }
-    println("$time ms")
+    println("\n Resolved in : $time ms")
 }
