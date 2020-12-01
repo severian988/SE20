a49ab793e6539dea7206f3b20ed58fa6b7a5f0fd
Leestalion <74027214+Leestalion@users.noreply.github.com>
2020-12-01 19:02:04 +0100

    adding a test on the super Poulet numbers
    This test verify if the pseudo primes super Poulet numbers are detected as prime or not. If they are not, the test is passed.

--- src/CheckerTest.kt
+++ src/CheckerTest.kt
@@ -17,9 +17,24 @@ class CheckerTest {
         }
         println("Test001 is OK!")
     }
+
+
+    /**
+     *  @author thomas 2020/12/01
+     * Check the super-Poulet numbers under 3000 are not a prime number.
+     */
+    fun CheckerTest02(){
+        val cnum = listOf(341L, 1387L, 2047L, 2701L, 3277L, 4033L, 4369L, 4681L, 5461L, 7957L, 8321L, 10261L, 13747L, 14491L, 15709L, 18721L, 19951L, 23377L, 31417L, 31609L, 31621L, 35333L, 42799L, 49141L, 49981L, 60701L, 60787L, 65077L, 65281L, 80581L, 83333L, 85489L, 88357L)
+        for (num in cnum){
+            assertFalse { c_.isPrime(num) }
+            assertFalse { c_.isPrimeRepunit(num.toBigInteger()) }
+        }
+        println("Test002 is OK!")
+    }
 }
 
 fun main(args: Array<String>){
     var ctest = CheckerTest()
     ctest.CheckerTest01()
+    ctest.CheckerTest02()
 }
